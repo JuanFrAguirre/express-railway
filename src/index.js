646a0e6acdd81182158908fd8783865c76b1dc66
Juan Francisco Aguirre <jf_aguirre@hotmail.com>
2022-11-18 13:28:25 -0300

    removed faulty endpoints

--- src/index.js
+++ src/index.js
@@ -26,32 +26,6 @@ app.get('/ping', async (req, res) => {
   res.send(result[0])
 })
 
-app.get('/create', async (req, res) => {
-  const result = await pool.query('INSERT INTO users(name) VALUES ("John")')
-  res.json(result)
-})
-
-app.get('/populate', async (req, res) => {
-  let scriptText = ''
-
-  JSON.parse(fs.readFileSync('data.json', 'utf-8')).forEach((x) => {
-    scriptText =
-      scriptText +
-      `\nINSERT INTO 
-    stores(name, address${x.phone ? ', phone' : ''}${
-        x.coordenates ? ', coordenates' : ''
-      }, loc, type, state${x.brand ? ', brand' : ''})
-    VALUES ("${x.name}", "${x.address}", ${x.phone ? `"${x.phone}", ` : ''}${
-        x.coordenates ? `"${x.coordenates}", ` : ''
-      }"${x.loc}", "${x.type}", "${x.state}"${
-        x.brand ? `, "${x.brand}"` : ''
-      });`
-  })
-
-  const [result] = await pool.query(scriptText)
-  res.json(result)
-})
-
 app.listen(PORT, () =>
   console.log('---\n\nServer listening on PORT ' + PORT + '\n\n---'),
 )
