d10230f674deb425ef96429dc8fa4fd61ee50ac3
Juan Francisco Aguirre <jf_aguirre@hotmail.com>
2022-11-19 07:32:25 -0300

    updated some endpoints

--- db/db.sql
+++ db/db.sql
@@ -1,3 +1,5 @@
+use usersdb;
+
 CREATE TABLE users (
   id INT PRIMARY KEY AUTO_INCREMENT,
   name VARCHAR(255) NOT NULL
@@ -13,4 +15,17 @@ CREATE TABLE stores (
   type VARCHAR(50) NOT NULL,
   state VARCHAR(50) NOT NULL,
   brand VARCHAR(50) NULL
+);
+
+create table products (
+	id int primary key auto_increment,
+	name varchar(50) not null,
+  photoUrl varchar(100) not null,
+  description varchar(1000) not null
+);
+
+insert into products (
+	name, photoUrl, description)
+values (
+	'Paño de limpieza','https://picsum.photos.com/200/200','La mejor opcion para la limpieza de tus lentes.'
 );
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import cors from 'cors'
 import * as dotenv from 'dotenv'
 import express from 'express'
-import fs from 'fs'
 import { pool } from './db.js'
 
 dotenv.config()
@@ -11,28 +10,38 @@ const PORT = process.env.PORT
 
 app.use(express.urlencoded()).use(express.json()).use(cors())
 
-app.get('/', (req, res) => {
+app.get('/', (_req, res) => {
   res.send("Try with '/users' or '/stores' !")
 })
 
-app.get('/users', async (req, res) => {
+app.get('/users', async (_req, res) => {
   const [result] = await pool.query('select * from users')
   res.json(result)
 })
 
-app.get('/stores', async (req, res) => {
+app.get('/stores', async (_req, res) => {
   const [result] = await pool.query('select * from stores')
   res.json(result)
 })
 
-app.get('/ping', async (req, res) => {
+app.get('/ping', async (_req, res) => {
   const [result] = await pool.query(`select "hello world" as result`)
   res.send(result[0])
 })
 
-app.get('/products', async (req, res) => {
+app.get('/products', async (_req, res) => {
   const [result] = await pool.query('select * from products')
   res.json(result)
+})
+
+app.post('/products', async (req, res) => {
+  const { name, photoUrl, description } = req.body
+  if (name && photoUrl && description) {
+    const [result] = await pool.query(`
+    INSERT INTO products (name, photoUrl, description)
+    values("${name}", "${photoUrl}", "${description}");`)
+    res.send(result)
+  } else res.status(500).send('missing required parameters')
 })
 
 app.listen(PORT, () =>
